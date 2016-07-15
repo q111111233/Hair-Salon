48fe09d846f3e6f8c7a86864264374366fd8aa15
Bo Zhao <zhaobo419@gmail.com>
2016-07-15 13:28:14 -0700

    build up Nancy app for client

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -20,6 +20,24 @@ namespace HairSalonList
         newStylist.Save();
         return View["index.cshtml", Stylist.GetAll()];
       };
+
+      // Delete["/"] = _ => {
+      //   Stylist SelectedStylist = Stylist.Find(parameters.id);
+      //   SelectedStylist.Delete();
+      //   return View["success.cshtml"];
+      // };
+
+      Get["/stylists/{id}"] = parameters => {
+        Stylist SelectedStylist = Stylist.Find(parameters.id);
+        return View["clients.cshtml", SelectedStylist];
+      };
+
+      Post["/stylists/{id}"] = parameters => {
+        Stylist SelectedStylist = Stylist.Find(parameters.id);
+        Client newClient = new Client(Request.Form["client-name"], parameters.id);
+        newClient.Save();
+        return View["clients.cshtml", SelectedStylist];
+      };
     }
   }
 }
